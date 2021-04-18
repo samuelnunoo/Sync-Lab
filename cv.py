acdcd2129d3fa270c26559fd73fcf8f0047a6899
lndiana <46731947+lndiana@users.noreply.github.com>
2021-04-18 16:23:08 -0700

    Add files via upload
    everything implemented. needs testing.

--- cv.py
+++ cv.py
@@ -25,7 +25,6 @@ lock = Lock()
 goth_friendly = Condition(lock)
 hipster_friendly = Condition(lock)
 
-
 # Too 
 def hangout():
     time.sleep(random.randint(0, 2))
@@ -36,9 +35,12 @@ class Club:
         self.goth_count = 0               # num goths in club
         self.hipster_count = 0            # num hipsters in club
         self.capacity = capacity          # only used for optional questions
-        self.num_attended = 0
-           
 
+        self.consec_goths = 0             # num consecutive goths joined
+        self.consec_hipsters = 0;         # num consecutive hipsters joined
+        self.max_consec = 3               # num consecutive allowed
+
+           
     def __sanitycheck(self):
         if self.goth_count > 0 and self.hipster_count > 0:
             print("sync error: bad social mixup! Goths = %d, Hipsters = %d" %  (self.goth_count, self.hipster_count))
@@ -53,56 +55,59 @@ class Club:
         
     def goth_enter(self):
         lock.acquire()
-        # if there are hipsters, wait
-        while (self.hipster_count > 0):
+
+        # wait while there are hipsters, too many goths have joined consecutively, or club is at capacity
+        while self.hipster_count > 0 or self.consec_goths >= self.max_consec or self.goth_count >= self.capacity:
             goth_friendly.wait()
 
-        self.goth_count +=1               
+        self.goth_count += 1               
+        self.consec_goths += 1
         self.__sanitycheck()
         lock.release()
 
 
     def goth_exit(self):
-
         lock.acquire()
+
         self.goth_count -= 1
+
+        # notify all hipsters if all goths have left, otherwise notify a goth
+        if self.goth_count == 0:
+            self.consec_goths = 0;
+            hipster_friendly.notify_all()
+        else:
+            goth_friendly.notify()
+
         self.__sanitycheck()
-
-        # if no more goths, notify all hipsters
-        if (self.goth_count == 0):
-            hipster_friendly.notify_all()
-
-        # if 6 goths have attended in a row, let hipsters join (CAN BE combined with above conditional)
-        self.num_attended += 1
-        if (self.num_attended % 6 == 0):
-            self.goth_count = 0
-            hipster_friendly.notify_all()
         lock.release()
 
 
     def hipster_enter(self):
-        
         lock.acquire()
-        # if there are goths, wait
-        while (self.goth_count > 0):
+
+        # wait while there are goths, too many hipsters have joined consecutively, or club is at capacity
+        while self.goth_count > 0 or self.consec_hipsters >= self.max_consec or self.hipster_count >= self.capacity:
             hipster_friendly.wait()
 
         self.hipster_count += 1
+        self.consec_hipsters += 1
         self.__sanitycheck()
-
         lock.release()
 
         
     def hipster_exit(self):
         lock.acquire()
+
         self.hipster_count -= 1
-        self.__sanitycheck()
 
-        # if no more hipsters, notify all goths
-        if (self.hipster_count == 0):
+        # notify all goths if all hipsters have left, otherwise notify a hipster
+        if self.hipster_count == 0:
+            self.consec_hipsters = 0;
             goth_friendly.notify_all()
+        else:
+            hipster_friendly.notify()
 
-        num_attended += 1
+        self.__sanitycheck()                  
         lock.release()
 
 
